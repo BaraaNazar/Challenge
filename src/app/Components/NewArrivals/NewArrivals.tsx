3698393b0c36c998f0e224a3d1b1abbf7623baf9
Baraa N. Mohsin <87875062+BaraaNazar@users.noreply.github.com>
2023-10-13 18:35:16 +0300

    Create a CategoryButton component

--- src/app/Components/NewArrivals/NewArrivals.tsx
+++ src/app/Components/NewArrivals/NewArrivals.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import CategoryButton from './CategoryButton/CategoryButton';
+import dataset from '../../../asset/dataset.json';
 
 const NewArrivals = () => {
   return (
-    <div className='flex justify-center items-center w-full py-6 font-serif'>
-      <section className='flex flex-col justify-center items-center text-center space-y-4 w-full'>
+    <div className='flex flex-col justify-center items-center w-full  font-serif'>
+      <section className='flex flex-col justify-center items-center text-center space-y-4 w-full py-8'>
         <h1 className='text-gray-700 text-4xl'>Miswag New Arrivals</h1>
-        <div className='w-[90%] text-xs font-sans '>
+        <div className='w-[90%] text-xs font-sans text-gray-500'>
           Stay Ahead of the Curve with Miswag&#39;g Fresh Arrivals!
         </div>
+      </section>
+      <section>
+        <ul className='flex justify-around items-center space-x-6 py-8'>
+          {dataset.map((data) => (
+            <CategoryButton key={data.id} categoryName={data.category} />
+          ))}
+        </ul>
       </section>
     </div>
   );
