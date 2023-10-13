23f45cef7319b35a42af04eabfea48aecf22b840
Baraa N. Mohsin <87875062+BaraaNazar@users.noreply.github.com>
2023-10-13 17:20:30 +0300

    Create a NewArrivals component

---
+++ src/app/Components/NewArrivals/NewArrivals.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NewArrivals = () => {
+  return (
+    <div className='flex justify-center items-center w-full py-6 font-serif'>
+      <section className='flex flex-col justify-center items-center text-center space-y-4 w-full'>
+        <h1 className='text-gray-700 text-4xl'>Miswag New Arrivals</h1>
+        <div className='w-[90%] text-xs font-sans '>
+          Stay Ahead of the Curve with Miswag&#39;g Fresh Arrivals!
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default NewArrivals;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import NewArrivals from './Components/NewArrivals/NewArrivals';
+
+export default function Home() {
+  return (
+    <div>
+      <NewArrivals />
+    </div>
+  );
+}
